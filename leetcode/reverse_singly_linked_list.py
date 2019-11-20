b9146792eea741e50a11a54e7a22d9b4ea386512
Farwent <philipp_ehm@protonmail.com>
2019-11-20 20:48:04 +0100

    LEETCODE 92. Reverse Linked List II (One Pass iterative Solution, O(n) Runtime Compl., O(1) Space Compl.)

--- leetcode/reverse_singly_linked_list.py
+++ leetcode/reverse_singly_linked_list.py
@@ -17,34 +17,53 @@ def reverse_list(head: ListNode) -> ListNode:
     return m
 
 
+def reverseBetween(head: ListNode, m: int, n: int) -> ListNode:
+    if not head or head.next is None or m is n: return head
+    ctr,l,mid,r,prev = 1,head,None,None,None
+
+    while ctr < m:
+        ctr += 1
+        prev = l
+        l = l.next
+    mid, r = l.next, l.next.next
+
+    while ctr < n-1:
+        mid.next = l
+        l,mid,r = mid,r,r.next
+        ctr += 1
+
+    mid.next = l
+    tail = r  #first node after the nth node in the linked list
+
+    if prev:
+        prev.next.next = tail
+        prev.next = mid
+    if not prev:
+        head.next = tail
+        head = mid
+
+    return head
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    a = ListNode(1)
-    b = ListNode(2)
-    c = ListNode(3)
-    d = ListNode(4)
-    e = ListNode(5)
-    f = ListNode(6)
-    g = ListNode(7)
-    h = ListNode(8)
-    i = ListNode(9)
-    j = ListNode(10)
-    k = ListNode(11)
-    l = ListNode(12)
+   a = ListNode(1)
+   b = ListNode(2)
+   c = ListNode(3)
+   d = ListNode(4)
+   e = ListNode(5)
 
-    a.next = b
-    b.next = c
-    c.next = d
-    d.next = e
-    e.next = f
-    f.next = g
-    g.next = h
-    h.next = i
-    i.next = j
-    j.next = k
-    k.next = l
+   a.next = b
+   b.next = c
+   c.next = d
+   d.next = e
 
-    res = reverse_list(None)
+   res = reverseBetween(a,1,2)
 
-    while res is not None:
-        print(res.val)
-        res = res.next
+   while res:
+       print(res.val)
+       res = res.next
