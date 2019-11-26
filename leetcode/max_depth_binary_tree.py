29a71c72c2208ff4bcfabaeb81700d627f0dba1d
Farwent <philipp_ehm@protonmail.com>
2019-11-26 18:15:31 +0100

    104. Maximum Depth of Binary Tree: Placeholder for further Impl
    - just4me, will impl this soon, assignement wise

---
+++ leetcode/max_depth_binary_tree.py
@@ -0,0 +1,14 @@
+#LEETOCDE: 104. Maximum Depth of Binary Tree
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def maxDepth(self, root: TreeNode) -> int:
+    print("PLACEHODLER")
+
+
+if __name__ == "__main__":
+    print()
