255caa3056b94fd17a51597a186290ccec1b3f8b
Farwent <philipp_ehm@protonmail.com>
2019-11-30 18:06:14 +0100

    144. Binary Tree Preorder Traversal - Iterative Approaches:
    - second pushes every node onto stack, first only the right child

--- leetcode/pre_order_traversal.py
+++ leetcode/pre_order_traversal.py
@@ -11,13 +11,12 @@ class TreeNode:
 """
 Recursive strategy: Pre-Order is: Root-Left-Right
 """
-
 #beats about 99% 20ms
-def preorderTraversal(root: TreeNode) -> [int]:
+def preorderTraversalI(root: TreeNode) -> [int]:
     if not root: return []
     res = [root.val]
-    res += preorderTraversal(root.left)
-    res += preorderTraversal(root.right)
+    res += preorderTraversalI(root.left)
+    res += preorderTraversalI(root.right)
     return res
 
 #beats about 90% 28ms
@@ -25,3 +24,36 @@ def preorderTraversalII(self, root: TreeNode) -> [int]:
     if not root: return []
     return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
 
+
+"""
+Iterative Solution: 32ms beats 75%. only pushes right child on temp stack
+"""
+def preorderTraversalIterative(root: TreeNode) -> [int]:
+    stack, res = [], []
+    current = root
+
+    while current:
+        res.append(current)
+
+        if current.left:
+            if current.right: stack.append(current.right)
+            res.append(current.left)
+            current = current.left
+        elif current.right:
+            current = current.right
+        else:
+            if stack: current = stack.pop()
+    return res
+
+#beats 90% 28ms, pushes every node onto the stack
+def preorderTraversalIterativeII(root: TreeNode) -> [int]:
+    if root is None: return []
+    stack, res = [root], []
+
+    while stack:
+        current = stack.pop()
+        res.append(current.val)
+
+        if current.right: stack.append(current.right)
+        if current.left: stack.append(current.left)
+    return res
