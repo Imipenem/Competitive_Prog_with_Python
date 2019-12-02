06662498553ed45552d737f63c1456e1bb9795e9
Farwent <philipp_ehm@protonmail.com>
2019-12-02 16:51:09 +0100

    Day 1: Advent of Code 1.2

--- adventofcode/day1.py
+++ adventofcode/day1.py
@@ -1,3 +1,6 @@
+"""
+Advent Of Code 1.1
+"""
 def computeFuel():
     sum,cnt = 0,0
     with open("/home/thelichking/Desktop/adventOfCode/Day1/Input Puzzle 1.1", 'r') as f:
@@ -10,5 +13,27 @@ def computeFuel():
     return sum
 
 
+"""
+Advent Of Code 1.2
+"""
+def computeFuelII():
+    sum,cnt = 0,0
+    with open("/home/thelichking/Desktop/adventOfCode/Day1/Input Puzzle 1.1", 'r') as f:
+        number = f.readline()
+        while number:
+            try:
+                sum += computeFuelRec(int(number.replace("\n", "")),0)
+                number = f.readline()
+            except:break
+    return sum
+
+
+def computeFuelRec(num, sum) -> int:
+    if num < 9: return sum
+    temp = int(num / 3) - 2
+    sum += temp
+    return computeFuelRec(temp, sum)
+
+
 if __name__ == "__main__":
-    print(computeFuel())
+    print(computeFuelII())
