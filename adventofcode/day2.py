05c0fb4352f7955cff94472473a37badd2bbacef
Farwent <philipp_ehm@protonmail.com>
2019-12-05 21:22:06 +0100

    Day 2: Advent of Code 2.2 - Total BruteForce

--- adventofcode/day2.py
+++ adventofcode/day2.py
@@ -2,7 +2,7 @@ def opcodeI() -> int:
     with open("/home/thelichking/Desktop/adventOfCode/Day2/Opcodes",'r' ) as file:
         lines = list(map(int,file.readline().replace("\n","").split(",")))
 
-        lines[1],lines[2] = 12,2
+        lines[1],lines[2] = 1,0
 
         idx = 0
 
@@ -17,5 +17,29 @@ def opcodeI() -> int:
         return lines[0]
 
 
+def opcodeII(noun, verb) -> int:
+    with open("/home/thelichking/Desktop/adventOfCode/Day2/Opcodes",'r' ) as file:
+        lines = list(map(int,file.readline().replace("\n","").split(",")))
+
+        lines[1],lines[2] = noun,verb
+
+        idx = 0
+
+        while idx < len(lines):
+            cur_opcode = lines[idx]
+            if cur_opcode == 99: return lines[0]
+
+            else:
+                if cur_opcode == 1: lines[lines[idx+3]] = lines[lines[idx+1]] + lines[lines[idx+2]]
+                elif cur_opcode == 2: lines[lines[idx+3]] = lines[lines[idx+1]] * lines[lines[idx+2]]
+                idx += 4
+        return lines[0]
+
+def inputII() -> int:
+    for i in range(100):
+        for j in range(100):
+            if opcodeII(j,i) == 19690720:
+                return 100*j + i
+
 if __name__=="__main__":
-    print(opcodeI())
+    print(inputII())
